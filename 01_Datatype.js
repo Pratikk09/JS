ad9714e634b18a34d6a0021facc4946eee5ba5f7
Pratikk09 <partikmehta29@gmail.com>
2025-03-08 10:11:49 +0530

    sec

---
+++ 01_Datatype.js
@@ -0,0 +1,13 @@
+// number => 1,2,3,4,5,6,7,8,9,10
+// string => "Pratik"
+// boolean => true/false
+// null => standalone value
+// undefined => undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "pratik")
+console.log(typeof 123)
+console.log(typeof true)
+console.log(typeof null)
