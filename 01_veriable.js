2912958a89eda85d5cf2d901b21571fbed6dc294
Pratikk09 <partikmehta29@gmail.com>
2025-03-08 09:34:12 +0530

    Initial commit

---
+++ 01_veriable.js
@@ -0,0 +1,10 @@
+const accountId = 1234
+let accountEmail = "9WbGw@example.com"
+var accountPassword = "123456"
+
+let accountState;
+// accountId = 2222
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword,accountState])
